038ede0f0e92387d578feb5d96d5ce59006a477f
YMarianaAguilar <ymarianaaguilar@gmail.com>
2023-09-21 21:14:20 -0300

    Agregamos productos al sitio de bisagras y carrito de compras

--- js/ecommerce.js
+++ js/ecommerce.js
@@ -1,99 +1,92 @@
+let carrito = [];
+
+function agregarCarrito (e){
+
+    console.log("Producto agregado al carrito", e.target);
+
+    let hijo = e.target;
+    let padre = hijo.parentNode;
+    let abuelo = padre.parentNode;
 
 
 
-class Bisagra{
+    let nombreProducto = padre.querySelector("h5").innerText;
+ 
 
-    constructor( nombre , precio , stock ){
-        this.nombre = nombre;
-        this.precio = precio;
-        this.stock = stock;
+    let precioProducto = padre.querySelector("span").innerText;
+
+
+    let imgProducto = abuelo.querySelector("img").src;
+
+
+    let producto = {
+        nombre: nombreProducto,
+        precio: precioProducto,
+        img: imgProducto,
+        cantidad:1
+    };
+
+    carrito.push(producto);
+
+    
+    mostrarCarrito();
+
+}
+
+function mostrarCarrito(){
+
+    let tabla = document.getElementById("tbody");
+
+    tabla.innerHTML = "";
+
+     for ( let producto of carrito ){
+
+         let fila = document.createElement("tr");
+         fila.innerHTML = `<td><img src="${producto.img}"></td>
+                           <td><p>${producto.nombre}</p></td>
+                           <td>${producto.cantidad}</td>
+                           <td>${producto.precio}</td>
+                           <td><button class="btn btn-danger btnBorrarProducto">Borrar</button></td>`;
+         tabla.append(fila);
+
+     }
+
+     let btnBorrar = document.querySelectorAll(".btnBorrarProducto");
+
+     for( let btn of btnBorrar ){
+         btn.addEventListener("click" , borrarProducto );
+     }
+
+}
+
+function borrarProducto(e){
+    console.log("BORRAR ESTE ELEMENTO: ", e.target );
+
+    let abuelo = e.target.parentNode.parentNode;
+    let productoEliminar = abuelo.querySelector("p").innerText;
+    console.log(productoEliminar);
+    abuelo.remove();
+
+    
+    function eliminarProducto( producto ){
+        return producto.nombre != productoEliminar
     }
 
-    get_datos(){
-        console.log("<-- BISAGRA -->");
-        console.log("Nombre: ", this.nombre);
-        console.log("Precio: ", this.precio);
-        console.log("Stock: ", this.stock);
-        console.log("");
-    }
-
-    get_stock(){
-        if ( this.stock <= 0) {
-            return false
-        }
-        else{
-            return true
-        }
-    }
-
-    update_stock( unidades ){
-        
-        if (this.stock >= unidades ) {
-            this.stock = this.stock - unidades;
-            return true
-        }
-        else{
-            console.log("No tenemos tanto stock");
-            console.log("Stock disponible: ", this.stock);
-            return false
-        }
-    }
+    let resultadoFilter = carrito.filter( eliminarProducto );
+    carrito = resultadoFilter
+    console.log(resultadoFilter)
 
 }
 
 
-let listaBisagras = []
+//EVENTOS
 
-listaBisagras.push( new Bisagra("Bisagras 1842", 880 , 7))
-listaBisagras.push( new Bisagra("Carrocera", 750 , 8))
-listaBisagras.push( new Bisagra("Municion", 900 , 9))
-listaBisagras.push( new Bisagra("Escalera", 720 , 8))
-listaBisagras.push( new Bisagra("Pestaña", 830 , 5))
+let btnCompra = document.querySelectorAll(".botonCompra");
 
-console.log("LISTA DE BISAGRAS");
+console.log(btnCompra);
 
-for( let bisagra of listaBisagras ){
 
-    bisagra.get_datos();
+for( let boton of btnCompra ){
 
-}
-
-function buscarBisagra( bisagra ){
-
-    return bisagra.nombre == compraUsuario
-
-}
-
-let compraUsuario = "";
-
-while ( compraUsuario != "FIN") {
-    compraUsuario = prompt("Ingrese el nombre de la bisagra que desee comprar");
-
-    if ( compraUsuario != "FIN") {
-        
-            let resultado_find = listaBisagras.find( buscarBisagra );
-
-            if ( resultado_find != undefined ) {
-                
-                    if ( resultado_find.get_stock() ) {
-                        
-                        let unidades = parseInt(prompt("¿Cuántas unidades desea comprar?"));
-
-                        if ( resultado_find.update_stock( unidades )) {
-                            console.log("Gracias por su compra");
-
-                            for( let bisagra of listaBisagras ){
-
-                                if ( bisagra.stock > 0 ) {
-                                    bisagra.get_datos();
-                                }
-                            }
-                        }
-                    }
-            }
-            else{
-                console.log("Bisagra no encontrada: ", compraUsuario);
-            }
-    }
-
+    boton.addEventListener("click" , agregarCarrito)
 }
